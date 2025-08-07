5fa5a214c10d80718723caba8acf5953183b0d02
Jean Paul Giraldo <jpgiraldo7@gmail.com>
2025-08-07 01:15:20 -0500

    Added a form to create new records

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "events#index"
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  resources :events
 end
