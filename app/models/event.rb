01e0aead4795ebb455e6b0e111e7045a15fd3d42
Jean Paul Giraldo <jpgiraldo7@gmail.com>
2025-09-24 21:55:43 -0500

    Added custom query to find upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
+  # Class method (that's why it uses "self.")
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")
+  end
+
+  # Instance method
   def free?
     # technically it should be self.price but self is implied here
     # because we're inside the model
